8497b34e2c9863e6847f906b85b4effd62aaf473
Michi Fusselball <mensing.michi@gmail.com>
2023-01-01 12:43:18 +0100

    feat: generate cards with text

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# astrotime
+# Astrotime
 Recreation of the astrotime boardgame for table top simulator published by Ravensburger (https://www.gesellschaftsspiele.de/ravensburger/astrotime/)
 
 
--- card-generation/generator.py
+++ card-generation/generator.py
@@ -1,8 +1,11 @@
 import os
 from os import path
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 import gettext
 from pathlib import Path
+import utils
 
 # constants
 LANGUAGES = ['de', 'en']
@@ -20,20 +23,18 @@ _ = t.gettext
 
 def generate_images():
 
-    # load front background image
-    front_image = Image.open(IMAGE_PATH + 'card_front.jpg')
-
-    print(LOCALE_DIR)
-
     for language in LANGUAGES:
         # load language files
         t = gettext.translation('base', localedir=LOCALE_DIR,
                                 languages=[language], fallback=True)
         t.install()
+
+        # load front background image
+        front_image = Image.open(IMAGE_PATH + 'card_front.jpg')
         generate_all_cards(language, front_image, t.gettext)
 
 
-def generate_all_cards(language: str, front_image, fn_gettext):
+def generate_all_cards(language: str, front_image: Image, fn_gettext):
 
     _ = fn_gettext
     # create target language folder
@@ -42,8 +43,33 @@ def generate_all_cards(language: str, front_image, fn_gettext):
         os.mkdir(IMAGE_PATH + language)
 
     # add text to card face
-    print(_('card_219_title'))
-    print(_('card_219_desc'))
+
+    draw = ImageDraw.Draw(front_image)
+    image_width = front_image.width
+    image_height = front_image.height
+
+    # show card id
+    font = ImageFont.truetype("tahoma.ttf", 16)
+    length = draw.textlength('219', font)
+    draw.text(((image_width - length)/2, image_height - 75), "219", (255, 255, 255),
+              font=font, align='center')
+
+    # show title
+    font = ImageFont.truetype("tahomabd.ttf", 35)
+    length = draw.textlength(_('card_219_title'), font)
+    draw.text(((image_width - length)/2, 200), _('card_219_title'), (255, 255, 255),
+              font=font, align='center')
+
+    # show scenario description
+    font = ImageFont.truetype("tahoma.ttf", 35)
+    utils.text_box(
+        _('card_219_desc'),
+        draw,
+        font,
+        (110, 300, image_width - 220, image_height),
+        utils.ALLIGNMENT_CENTER,
+        utils.ALLIGNMENT_TOP
+    )
 
     # save image in language folder
     front_image.save(IMAGE_PATH + language + '/' + card_name('219', True))
