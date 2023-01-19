cb0eabd5794a4f021551889f07bff22e6867c6ff
Michi Fusselball <mensing.michi@gmail.com>
2023-01-18 21:37:48 -0500

    feat: extract card from scans

--- card-generation/read_card.py
+++ card-generation/read_card.py
@@ -1,20 +1,24 @@
 
 from PIL import Image
+from math import sqrt
 from import_from_excel import generate_i18n_files
 from pytesseract import pytesseract
+import uuid
 
 path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 pytesseract.pytesseract_cmd = path_to_tesseract
 
 # color id, coordinates, color rgb
-COLOR_ARRAY = [('red', (60, 120), (173, 15, 14)),
-               ('yellow', (150, 120), (239, 199, 3)),
-               ('green', (210, 120), (1, 155, 33)),
-               ('blue', (510, 120), (84, 154, 250)),
-               ('grey', (570, 120), (131, 131, 131)),
-               ('purple', (640, 120), (106, 0, 154))]
+COLOR_ARRAY = [
+    ('green', (60, 120), (53, 150, 53)),
+    ('red', (150, 120), (191, 32, 29)),
+    ('yellow', (210, 120), (255, 248, 79)),
+    ('purple', (510, 120), (114, 78, 124)),
+    ('grey', (570, 120), (204, 230, 195)),
+    ('blue', (640, 120), (47, 164, 218))
+]
 
-ATTRIBUTE_COLOR = (239, 199, 3)
+ATTRIBUTE_COLOR = (254, 247, 96)
 
 
 def read_front_image(image_path, result_i18n):
@@ -34,10 +38,8 @@ def read_front_image(image_path, result_i18n):
 
     # read id
     id_area_image = image.crop(
-        (4*margin, height-1.5*margin, width-4*margin, height-0.5*margin))
-
-    id_text = pytesseract.image_to_string(id_area_image, lang='deu')
-    id = ''.join(id_text).strip()
+        (4*margin, height-1.5*margin, width-4.1*margin, height-0.5*margin))
+    id = read_id_number(id_area_image)
 
     generate_i18n_files((id, title, scenario), result_i18n)
 
@@ -50,19 +52,18 @@ def read_back_image(image_path, result_cards):
     width = image.width
     margin = 75
     id_area_image = image.crop(
-        (4*margin, 0.5*margin, width-4*margin, 2*margin))
+        (4.1*margin, 0.7*margin, width-4.2*margin, 1.7*margin))
 
     # read id
-    id_text = pytesseract.image_to_string(id_area_image, lang='deu')
-    card['id'] = id_text
+    card['id'] = read_id_number(id_area_image)
 
     # read color markers
     for id, coords, color in COLOR_ARRAY:
         card[id] = is_color_present(image, coords, color)
 
     # read attribute pattern
-    band_height = 71
-    mid_offset = 20
+    band_height = 73
+    mid_offset = 30
     for i in range(20):
         row = i % 10
         if i < 10:
@@ -81,7 +82,27 @@ def read_back_image(image_path, result_cards):
 
 def is_color_present(image, coordinates, color) -> bool:
     pix = image.getpixel(coordinates)
-    return True if pix == color else False
+    image.putpixel(coordinates, (0, 255, 0))
+
+    # euclidean distance
+    d = sqrt(pow(pix[0] - color[0], 2) + pow(pix[1] -
+             color[1], 2) + pow(pix[2] - color[2], 2))
+    return True if d < 40 else False
+
+
+def read_id_number(image) -> str:
+    # grayscale
+    id_area_image = image.convert('L')
+    # Threshold
+    id_area_image = id_area_image.point(lambda p: 255 if p > 100 else 0)
+    # To mono
+    id_area_image = id_area_image.convert('1')
+
+    id_text = pytesseract.image_to_string(
+        id_area_image, lang='eng', config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
+    id_area_image.save('crop.jpg')
+
+    return ''.join(id_text).strip() if id_text != '' else str(uuid.uuid4())
 
 
 def get_default_card():
@@ -117,9 +138,9 @@ def get_default_card():
 
 
 result_i18n = []
-read_front_image('../images/de/1_front.jpg', result_i18n)
+read_front_image('./test_0.jpg', result_i18n)
 result_cards = []
-read_back_image('../images/de/4_back.jpg', result_cards)
+read_back_image('./test_11.jpg', result_cards)
 
 print(result_i18n)
 print(result_cards)
---
+++ card-generation/read_card_scans.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+
+
+def detect_crop_images(card_images, file_path):
+    image = cv2.imread(file_path)
+
+    # turn to binary image
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thres_image = cv2.threshold(gray_image, 250, 255, cv2.THRESH_BINARY)
+
+    # remove noice fragments by erosion and dilation
+    se1 = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
+    se2 = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 20))
+    thres_image = cv2.morphologyEx(thres_image, cv2.MORPH_CLOSE, se1)
+    thres_image = cv2.morphologyEx(thres_image, cv2.MORPH_OPEN, se2)
+
+    # find contours
+    contours, _ = cv2.findContours(
+        thres_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+
+        # get rotated bounding box
+        rect = cv2.minAreaRect(contour)
+        center = rect[0]
+        size = rect[1]
+        angle = rect[2]
+
+        # skip contours that are too large or small
+        if size[0] > 1500 or size[0] < 500:
+            continue
+
+        center, size = tuple(map(int, center)), tuple(map(int, size))
+        height, width = image.shape[0], image.shape[1]
+
+        # rotate image around center of bounding box, correct if detected as horizontal
+        if size[0] > 850:
+            size = (size[1], size[0])
+            angle = angle + 90
+        M = cv2.getRotationMatrix2D(center, angle, 1)
+        img_rot = cv2.warpAffine(image, M, (width, height))
+
+        img_crop = cv2.getRectSubPix(img_rot, size, center)
+
+        card_images.append(img_crop)
+
+
+card_images = []
+files = ['../images/cards/cards_000_front.jpg',
+         '../images/cards/cards_001_back.jpg']
+
+for path in files:
+    detect_crop_images(card_images, path)
+
+for i, image in enumerate(card_images):
+    cv2.imwrite('./test_{0}.jpg'.format(i), image)
